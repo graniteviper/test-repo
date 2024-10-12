f29fc84a6c5ed4d52d6916fc4bc7284ee7cfd0d8
graniteviper <tanishqs1403@gmail.com>
2024-10-12 17:56:06 +0530

    Adding the starter files for web3 100xDevs course

---
+++ week-1-web3.js
@@ -0,0 +1,46 @@
+/*
+import crypto from "crypto";
+
+let hash = "";
+let input = 0;
+while(true){
+  let inputstr = input.toString();
+  hash = crypto.createHash('sha256').update(inputstr).digest('hex')
+  if(hash.startsWith("00000")){
+    console.log("input:",input,'\n',"hash:",hash);
+    break;
+  }
+  input++;
+}
+
+
+
+let hash = "";
+let input = 0;
+while(true){
+  let inputstr = "100xdevs" + input.toString();
+  hash = crypto.createHash('sha256').update(inputstr).digest('hex')
+  if(hash.startsWith("00000")){
+    console.log("input:",input,'\n',"hash:",hash);
+    break;
+  }
+  input++;
+}
+
+
+
+
+
+
+let hash = "";
+let input = 0;
+while(true){
+  let inputstr = "harkirat => Raman | Rs 100 Ram => Ankit | Rs 10"+input.toString();
+  hash = crypto.createHash('sha256').update(inputstr).digest('hex')
+  if(hash.startsWith("00000")){
+    console.log("input:",input,'\n',"hash:",hash);
+    break;
+  }
+  input++;
+}
+  */
